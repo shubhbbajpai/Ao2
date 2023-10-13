9d447ed7d726329f75a2ab144894f690a0200c0b
sbaj0015 <sbaj0015@student.monash.edu>
2023-10-13 15:21:01 +1100

    Completion of the Mountain Organizer task.

--- mountain.py
+++ mountain.py
@@ -7,3 +7,48 @@ class Mountain:
     name: str
     difficulty_level: int
     length: int
+
+    #if both mountains are equal to each other.
+    def __eq__(self, other):
+
+        if self.difficulty_level == other.difficulty_level:
+            if self.name == other.name:
+                return self.name == other.name
+        return False
+    
+    #if both mountains are less than to each other.
+    def __lt__(self, other):
+        if self.difficulty_level < other.difficulty_level:
+            return self.difficulty_level < other.difficulty_level
+        
+        elif self.difficulty_level > other.difficulty_level:
+            return False
+        
+        elif self.difficulty_level == other.difficulty_level:
+            return self.name < other.name
+
+    #if both mountains are greater than to each other. 
+    def __gt__(self, other):
+        
+        if self.difficulty_level > other.difficulty_level:
+            return self.difficulty_level > other.difficulty_level
+        
+        elif self.difficulty_level < other.difficulty_level:
+            return False
+        
+        elif self.difficulty_level == other.difficulty_level:
+            return self.name > other.name
+        
+    def __le__(self, other):
+        if self.difficulty_level < other.difficulty_level:
+            return True
+        if self.difficulty_level == other.difficulty_level:
+            return self.name <= other.name
+        return False
+        
+    def __ge__(self, other):
+        if self.difficulty_level > other.difficulty_level:
+            return True
+        if self.difficulty_level == other.difficulty_level:
+            return self.name >= other.name
+        return False
--- mountain_organiser.py
+++ mountain_organiser.py
@@ -2,13 +2,27 @@ from __future__ import annotations
 
 from mountain import Mountain
 
+from algorithms import binary_search, mergesort
+
 class MountainOrganiser:
 
     def __init__(self) -> None:
-        raise NotImplementedError()
+
+        self.mountain_list = []
+        self.mountain_rankings = {}
+        self.mountain_count = 0
 
     def cur_position(self, mountain: Mountain) -> int:
-        raise NotImplementedError()
+        
+        #complexity of the mountain_ranking is O(log(N)), where N is the total # of mountains
+        mountain_ranking = binary_search.binary_search(self.mountain_list, mountain)
+
+        if  self.mountain_list[mountain_ranking] != mountain:
+            raise KeyError("Error. The mountain was not found in the list.")
+        
+        return mountain_ranking
 
     def add_mountains(self, mountains: list[Mountain]) -> None:
-        raise NotImplementedError()
+
+        sorted_mountain_list = mergesort.mergesort(mountains)
+        self.mountain_list = mergesort.merge(self.mountain_list, sorted_mountain_list)
